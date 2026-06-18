0ea16648fa01b8e1fe152af2fe21158a0ab24bba
Carlos Finney <finney.carlossouza@gmail.com>
2026-06-18 20:38:18 -0300

    Estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("====================================")
+    print("      Bem-vindo ao CyberToolkit")
+    print("====================================")
+
+if __name__ == "__main__":
+    main()
